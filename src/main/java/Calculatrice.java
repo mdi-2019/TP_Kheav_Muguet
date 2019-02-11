99684f26811ff8be77aed7140dff73f7140b4e27
nicolas muguet <16004218@e105m03.istic.univ-rennes1.fr>
2019-02-11 15:42:13 +0100

    ajout choix mode

--- src/main/java/Calculatrice.java
+++ src/main/java/Calculatrice.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 
 public class Calculatrice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char c=' ';
+		boolean scientifique = false;
+		while(true) {
+			System.out.println("Choisir le mode : (c)lassique | (s)cientifique");
+			c = sc.nextLine().charAt(0);
+			if (c=='c') {
+				scientifique = false;
+				break;
+			}
+			else if (c=='s') {
+				scientifique = true;
+				break;
+			}
+		}
 
 	}
 
